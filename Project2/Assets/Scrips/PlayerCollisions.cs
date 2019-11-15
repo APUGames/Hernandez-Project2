fcd9f6ffef19cb5dc94769042acff4b35b44b871
APU Cinema <cinemasupport@apu.edu>
2019-11-15 15:32:05 -0800

    New Docs, new colliders with shack

---
+++ Project2/Assets/Scrips/PlayerCollisions.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisions : MonoBehaviour
+{
+    // Logic or Shack Door
+    private bool doorIsOpen = false;
+    private float doorTimer = 0.0f;
+    public float doorOpenTime = 3.0f;
+
+
+    // Door Sounds 
+    public AudioClip doorOpenSound;
+    public AudioClip doorCloseSound;
+    private new AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Timer that automaticlly shuts door once it's open
+        if(doorIsOpen)
+        {
+            doorTimer += Time.deltaTime; 
+        }
+        if(doorTimer > doorOpenTime)
+        {
+            ShutDoor();
+            doorTimer = 0.0f;
+        }
+    }
+
+    // Collision detection 
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "shackDoor" && !doorIsOpen) 
+        {
+            OpenDoor();
+        }
+
+    }
+
+    void OpenDoor()
+    {
+        // Play audio
+        audio.PlayOneShot(doorOpenSound);
+        // Set doorIsOpen to true
+        doorIsOpen = true;
+        // Find the GameObject that has animation
+        GameObject myShack = GameObject.Find("Shack");
+        // Play animation 
+        myShack.GetComponent<Animation>().Play("doorOpen");
+    }
+
+
+
+    void ShutDoor()
+    {
+
+        audio.PlayOneShot(doorCloseSound);
+        doorIsOpen = false;
+        GameObject myShack = GameObject.Find("Shack");
+        myShack.GetComponent<Animation>().Play("doorShut");
+    }
+
+
+}
